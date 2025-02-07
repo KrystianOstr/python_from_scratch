87423a270fa163687499242ffc3a4db97e4dd29a
Krystian-O <krystian.ostrogorski@gmail.com>
2025-02-07 14:51:52 +0000

    next commit

---
+++ Basics, Flow, Loops/F1.py
@@ -0,0 +1,12 @@
+# money_available = 10
+
+# if money_available >= 80:
+#     print('eat something fancy')
+# elif money_available > 45:
+#     print('eat something nice')
+# elif money_available > 15:
+#     print('eat something okay')
+# else:
+#     print('eat something cheap')
+
+# ------------------------------
---
+++ Basics, Flow, Loops/F2.py
@@ -0,0 +1,17 @@
+# mood = 'none'
+
+# match mood:
+#     case 'happy':
+#         print('happy')
+#     case 'sad':
+#         print('sad')
+
+# grade = str(5)
+
+# match grade:
+#     case '1':
+#         print('Popraw to')
+#     case '5':
+#         print('niezle')
+#     case _:
+#         print('jest ok')
