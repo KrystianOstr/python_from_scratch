8e244d8aa2c5fe68f89608406d56e4939c008cb5
Krystian <krystian.ostrogorski@gmail.com>
2025-02-09 21:19:01 +0100

    loops

---
+++ Basics, Flow, Loops/Loops.py
@@ -0,0 +1,20 @@
+# # WHILE LOOP
+
+# x = 0
+# my_list = []
+
+# while x < 100:
+#     x += 1
+
+#     if x%2 == 0:
+#         if x == 58:
+#             continue
+        
+#         my_list.append(x)
+
+
+
+# print(my_list)
+
+# FOR LOOP
+
